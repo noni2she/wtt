14538bf6407e86fc47c1d03303696212141bbc5f
Chun-MingChen <nick830314@gmail.com>
2017-06-23 23:18:38 +0800

    allow login flow configurable for shell

--- back-end/src/containers/App.jsx
+++ back-end/src/containers/App.jsx
@@ -1,13 +1,24 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import Login from './login.jsx';
+
 class App extends React.Component {
-  constructor() {
-    super();
-  }
   render() {
-    return (
+    // redux variable loginComfirmed
+    const loginComfirmed = false;
+    const loginFlowEnable = 
+      process.env.NODE_ENV === 'development' &&
+      process.env.REACT_APP_LOGIN === 'false';
+     
+    // if user enter the correct account and password set or disable login flow for development need
+    // skip the login.
+    return loginComfirmed || loginFlowEnable ? (
       <div>
-        {this.props.children}
+        { this.props.children }
+      </div>
+    ) : (
+      <div> 
+        <Login />
       </div>
     );
   }
