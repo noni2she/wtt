36364e391bb645f9afe0909881e0deed0052a092
Yihau-Chen <a122092487@gmail.com>
2017-06-26 00:12:15 +0800

    ues camelCase

---
+++ back-end/src/components/topBanner/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Carousel from './carousel.jsx'
+
+const fakedata = [
+  {
+    url:'https://firebasestorage.googleapis.com/v0/b/techwell-74c86.appspot.com/o/top_banner%2FAD_1.png?alt=media&token=891d542c-969d-402c-94b2-a43d0eef4c18',
+    alt:'first'
+  },
+  {
+    url:'https://firebasestorage.googleapis.com/v0/b/techwell-74c86.appspot.com/o/top_banner%2FAD_2.png?alt=media&token=1077c8a3-6bf0-4b70-933f-7ac2c6edbd8f',
+    alt:'second'
+  }
+];
+
+export default class TopBanner extends Component {
+  render () {
+    return (
+      <div className="top-banner">
+        <Carousel bannerInfo={fakedata}/>
+      </div>
+    );
+  }  
+}
