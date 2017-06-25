ff2e17ec31b6aa0dd53f06063873355970f9ce48
Chun-MingChen <nick830314@gmail.com>
2017-06-26 01:16:24 +0800

    add 'get' and 'update' api for firebase

--- back-end/src/utils/firebase.js
+++ back-end/src/utils/firebase.js
@@ -11,7 +11,46 @@ export const getFirebase = () => {
   return firebase;
 }
 
-// get firebase database object
-export const getFirebaseDatabase = () => {
-  return firebase.database();
+/* set() 
+ * to save data to a specified reference, including any child nodes. 
+ * Replace any existing data at that path.
+ */
+export const setData = (url, data) => {
+  // prevent empty or undefined.
+  if (typeof url !== 'string' || typeof data !== 'object') return;
+  
+  // return true that means save successfully.
+  return firebase.database().ref(url).set(data)
+    .then((result) => {
+      return true;
+    })
+    .catch((error) => {
+      console.error(error);
+      return false;
+    });
+}
+
+/* once() 
+ * Read data once.
+ * retrieve the data in the snapshot with the val() method.
+ * 
+ * @event: 
+ *  'value' is used to Read and listen for changes to the entire contents of a path.
+ * 
+ * @return:
+ *   return the Object which store under that path. 
+ */
+export const getData = (url) => {
+  // prevent empty or undefined.
+  if (typeof url !== 'string') return;
+
+  return firebase.database().ref(url).once('value')
+    .then((snapshot) => {
+      return snapshot.val();
+    })
+    .catch((error) => {
+      // return false that means get nothing back.
+      console.error(error);
+      return false;
+    });
 }
