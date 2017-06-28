e68f9217bfec08a80e1cafb77e6ee67729045797
Yihau-Chen <a122092487@gmail.com>
2017-06-28 22:42:18 +0800

    delete carousel.jsx

--- back-end/src/components/topBanner/index.jsx
+++ back-end/src/components/topBanner/index.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import Carousel from './carousel.jsx';
+import Slider from 'react-slick';
+import Banner from '../common/imgItem.jsx';
+
 import { contentObject } from '../../utils/fakeData';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  centerMode: true
+};
 
 export default class TopBanner extends Component {
   render () {
+    const BannerList = this.props.imgItems.map((item, index) => <div><Banner imgItem={item} key={'banner_'+index}/></div>);
     return (
       <div className="top-banner">
-        <Carousel imgItems={contentObject.topBanner.imgItems}/>
+        <Slider {...settings}>
+          {BannerList}
+        </Slider>
       </div>
     );
   }  
