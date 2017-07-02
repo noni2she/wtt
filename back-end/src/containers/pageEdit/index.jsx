46df58316ce5af91d0efa7b98c711d2dd7b5c3ad
Chun-MingChen <nick830314@gmail.com>
2017-07-03 00:20:26 +0800

    PageEdit initialize

---
+++ back-end/src/containers/pageEdit/index.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import NavBar from '../../components/common/navBar.jsx';
+
+class PageEdit extends Component {
+  render() {
+    return (
+      <div className="container-with-nav-bar">
+        <NavBar />
+
+        <div id="page-edit" className="container">
+          <form>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" /> Displayed
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputFile">File input</label>
+              <input type="file" id="exampleInputFile" />
+              <p className="help-block">Example block-level help text here.</p>
+            </div>
+            
+            <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default PageEdit;
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+
 import App from 'containers/App.jsx';
 import PageIndex from 'containers/pageIndex/index.jsx';
 import PageProduct from 'containers/pageProduct/index.jsx';
+import PageEdit from 'containers/pageEdit/index.jsx';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PageIndex}/>
-    <Route path="/edit/:index" component={PageIndex}/>
+    <Route path="/edit/:index" component={PageEdit}/>
     <Route path="/product/:categoryKey/:seriesKey" component={PageProduct}/>
   </Route>
 );
