f53b6a82bb24f3c08f494735e8f3ff4b3439316e
Chun-MingChen <nick830314@gmail.com>
2017-07-03 23:51:00 +0800

    import component with alias path

--- back-end/src/containers/pageEdit/index.jsx
+++ back-end/src/containers/pageEdit/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NavBar from '../../components/common/navBar.jsx';
+import NavBar from 'components/common/navBar.jsx';
 
 class PageEdit extends Component {
   render() {
--- back-end/src/containers/pageProduct/index.jsx
+++ back-end/src/containers/pageProduct/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { NAV_BAR_PRODUCTS } from '../../constants/common';
+import { NAV_BAR_PRODUCTS } from 'constants/common';
 
 // component  
 import NavBar from 'components/common/navBar.jsx';
