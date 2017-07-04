0ae23a8e778186ccb6897bc0388c1a9443ea8c3c
Yihau-Chen <a122092487@gmail.com>
2017-07-04 19:42:03 +0800

    use deconstructor

--- back-end/src/components/contact/index.jsx
+++ back-end/src/components/contact/index.jsx
@@ -3,40 +3,43 @@ import ImgItem from '../common/imgItem.jsx';
 
 export default class Contact extends Component {
   render () {
-    const about = this.props.about;
+    const {
+      header, subheader, subImg, company,
+      phone, fax, email, location, mainImg
+    } = this.props.about;
     return (
       <div id="contact-div" className="container">
         <div>
-          <h2 className="text-uppercase" >{about.header}</h2>
-          <p>{about.subheader}</p>
+          <h2 className="text-uppercase" >{header}</h2>
+          <p>{subheader}</p>
         </div>
         <div className="company-information">
           <div className="company-introduction col-lg-5">
-            <ImgItem imgItem={about.subImg} />
-            <h4>{about.company}</h4>
+            <ImgItem imgItem={subImg} />
+            <h4>{company}</h4>
             <div>
               <ul className="list-unstyled">
                 <li>
                   <i className="glyphicon glyphicon-earphone"></i>
-                  <span>{about.phone}</span>
+                  <span>{phone}</span>
                 </li>
                 <li>
                   <i className="glyphicon glyphicon-print"></i>
-                  <span>{about.fax}</span>
+                  <span>{fax}</span>
                 </li>
                 <li>
                   <i className="glyphicon glyphicon-envelope"></i>
-                  <span>{about.email}</span>
+                  <span>{email}</span>
                 </li>
                 <li>
                   <i className="glyphicon glyphicon-map-marker"></i>
-                  <span>{about.location}</span>
+                  <span>{location}</span>
                 </li>
               </ul>
             </div>
           </div>
           <div className="company-map col-lg-7">
-            <ImgItem imgItem={about.mainImg} />
+            <ImgItem imgItem={mainImg} />
           </div>
         </div>
       </div>
--- back-end/src/components/download/downloadItem.jsx
+++ back-end/src/components/download/downloadItem.jsx
@@ -3,9 +3,11 @@ import ImgItem from '../common/imgItem.jsx';
 
 export default class Download extends Component {
   render () {
-  const downloadItem = this.props.downloadItem;
-  const downloadItemDescription = downloadItem.description.map((item, index) => <span key={'downItemDescription_'+index}>{item}</span>);
-  const downloadItemLink = downloadItem.link.map((item, index) => {
+  const {
+    description, link, mainImg, title
+  } = this.props.downloadItem;
+  const downloadItemDescription = description.map((item, index) => <span key={ 'downItemDescription_' + index }>{item}</span>);
+  const downloadItemLink = link.map((item, index) => {
     return(
       <li key={'downItemlink_'+index}>
         <span className="">{Object.keys(item)[0]}:</span>
@@ -16,18 +18,18 @@ export default class Download extends Component {
     return (
       <div className="download-item row">
         <div className="download-img col-lg-3 col-lg-offset-1">
-          <ImgItem imgItem={downloadItem.mainImg}/>
+          <ImgItem imgItem={mainImg}/>
         </div>
         <div className="download-description col-lg-4">
-          <span className="download-description-title"><i className="glyphicon glyphicon-file" />{downloadItem.title}</span>
+          <span className="download-description-title"><i className="glyphicon glyphicon-file" />{title}</span>
           <ul className="list-unstyled">
-            { downloadItemDescription }
+            {downloadItemDescription}
           </ul>
         </div>
         <div className="download-link col-lg-3">
           <i className="glyphicon glyphicon-download-alt"></i>
           <ul className="list-unstyled">
-            { downloadItemLink }
+            {downloadItemLink}
           </ul>
         </div>
       </div>
