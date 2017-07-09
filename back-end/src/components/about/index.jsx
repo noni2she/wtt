f636cf5e34741ea14c18d4e51f6137f3f69b712d
Yihau-Chen <a122092487@gmail.com>
2017-07-09 13:46:56 +0800

    typesetting

--- back-end/src/components/about/index.jsx
+++ back-end/src/components/about/index.jsx
@@ -4,8 +4,8 @@ export default class About extends Component {
   render () {
     const {header, description} = this.props.about;
     return (
-      <div id="about">
-        <div className="about-container">
+      <div id="about" className="container-fluid">
+        <div className="about-container col-lg-10 col-md-10 col-sm-10 col-xs-10">
           <div className="about-title">
             <h1 className="text-uppercase">{header}</h1>
           </div>
