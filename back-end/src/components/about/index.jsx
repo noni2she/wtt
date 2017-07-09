7e9f13ec94b51e09bed0f30bedf66f25d5a4a108
Yihau-Chen <a122092487@gmail.com>
2017-07-09 15:57:02 +0800

    edit link inner div

--- back-end/src/components/about/index.jsx
+++ back-end/src/components/about/index.jsx
@@ -6,17 +6,17 @@ export default class About extends Component {
     const {header, description} = this.props.about;
     return (
       <div id="about" className="container-fluid">
-        <Link to={'/edit/about'}>
-          <div className="about-container col-lg-10 col-md-10 col-sm-10 col-xs-10">
+        <div className="about-container col-lg-10 col-md-10 col-sm-10 col-xs-10">
+          <Link to={'/edit/about'}>
             <div className="about-title">
               <h1>{header}</h1>
             </div>
             <div className="about-content">
               <p>{description}</p>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
     );
-  }  
-} 
+  }
+}
