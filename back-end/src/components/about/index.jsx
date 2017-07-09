200a409fac1d3fad000a7a56660da5361875ccdf
Yihau-Chen <a122092487@gmail.com>
2017-07-09 15:41:33 +0800

    use link to wrap div

--- back-end/src/components/about/index.jsx
+++ back-end/src/components/about/index.jsx
@@ -6,16 +6,16 @@ export default class About extends Component {
     const {header, description} = this.props.about;
     return (
       <div id="about" className="container-fluid">
-        <div className="about-container col-lg-10 col-md-10 col-sm-10 col-xs-10">
-          <Link to={'/edit/about'}>
+        <Link to={'/edit/about'}>
+          <div className="about-container col-lg-10 col-md-10 col-sm-10 col-xs-10">
             <div className="about-title">
-              <h1 className="text-uppercase">{header}</h1>
+              <h1>{header}</h1>
             </div>
             <div className="about-content">
               <p>{description}</p>
             </div>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     );
   }  
