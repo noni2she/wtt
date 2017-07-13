26a84465bd0044ff87ac6ca3cea18039c9c45f55
Chun-MingChen <nick830314@gmail.com>
2017-07-13 20:21:10 +0800

    change attribute key to downloadItems and tunning for better coding style

--- back-end/src/components/download/index.jsx
+++ back-end/src/components/download/index.jsx
@@ -4,8 +4,8 @@ import DownloadItem from './downloadItem.jsx';
 
 export default class Download extends Component {
   render () {
-    const download = this.props.download;
-    const downloadList = download.downloadItem.map((item, index) => {
+    const { downloadItems, header, subheader } = this.props.download;
+    const downloadList = downloadItems.map((item, index) => {
       return (
         <DownloadItem
           key={`downItem_${index}`}
@@ -19,8 +19,8 @@ export default class Download extends Component {
       <div id="download" className=".container-fluid">
         <Link to={'/edit/download/header'}>
           <div>
-            <h2>{download.header}</h2>
-            <p>{download.subheader}</p>
+            <h2>{header}</h2>
+            <p>{subheader}</p>
           </div>
         </Link>
         { downloadList }
