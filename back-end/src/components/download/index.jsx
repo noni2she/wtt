6f537080ce244cb770633c999c8a95a022014628
Chun-MingChen <nick830314@gmail.com>
2017-07-13 20:13:41 +0800

    navigate to edit page for downloadItem when clicking

--- back-end/src/components/download/index.jsx
+++ back-end/src/components/download/index.jsx
@@ -5,7 +5,16 @@ import DownloadItem from './downloadItem.jsx';
 export default class Download extends Component {
   render () {
     const download = this.props.download;
-    const downloadList = download.downloadItem.map((item, index) => <DownloadItem downloadItem={item} key={'downItem_'+index}/>);
+    const downloadList = download.downloadItem.map((item, index) => {
+      return (
+        <DownloadItem
+          key={`downItem_${index}`}
+          downloadItem={item}
+          downloadIndex={index}
+        />
+      )
+    });
+
     return (
       <div id="download" className=".container-fluid">
         <Link to={'/edit/download/header'}>
