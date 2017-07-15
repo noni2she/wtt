939668e313b3697ce6a57769d880398c84fcabd4
Yihau-Chen <a122092487@gmail.com>
2017-07-15 13:32:12 +0800

    remove link and fixed coding style

--- front-end/app/components/about/index.jsx
+++ front-end/app/components/about/index.jsx
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 export default class About extends Component {
-  render () {
-    const {header, description} = this.props.about;
+  render() {
+    const { header, description } = this.props.about;
     return (
       <div id="about" className="container-fluid">
-        <div className="about-container col-lg-10 col-md-10 col-sm-10 col-xs-10">
-          <Link to={'/edit/about'}>
+        <div className="about-container col-lg-10 col-md-10 col-sm-12 col-xs-12">
             <div className="about-title">
               <h1>{header}</h1>
             </div>
             <div className="about-content">
               <p>{description}</p>
             </div>
-          </Link>
         </div>
       </div>
     );
