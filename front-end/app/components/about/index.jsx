2c2be624414f0258a67eb368908059013c43aa29
Yihau-Chen <a122092487@gmail.com>
2017-07-15 15:29:44 +0800

    remove containner-fluid

--- front-end/app/components/about/index.jsx
+++ front-end/app/components/about/index.jsx
@@ -5,7 +5,7 @@ export default class About extends Component {
   render() {
     const { header, description } = this.props.about;
     return (
-      <div id="about" className="container-fluid">
+      <div id="about">
         <div className="about-container col-lg-10 col-md-10 col-sm-12 col-xs-12">
             <div className="about-title">
               <h1>{header}</h1>
