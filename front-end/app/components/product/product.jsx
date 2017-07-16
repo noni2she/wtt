202a8b6ec2b8bb46c0005242278814e5c7717f86
Yihau-Chen <a122092487@gmail.com>
2017-07-16 13:34:56 +0800

    remove unused code and add props validation

--- front-end/app/components/product/product.jsx
+++ front-end/app/components/product/product.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import ImgItem from 'components/common/imgItem.jsx';
 import { SERIES_ITEM_COUNT_PER_ROW } from 'constants/common';
 
-export class ProductItem extends Component {
+export default class ProductItem extends Component {
 
   constructor() {
     super();
@@ -14,15 +15,14 @@ export class ProductItem extends Component {
     const { categoryKey } = this.props;
     const { seriesItems } = this.props.categoryItem;
 
-    let seriesList1 = [];
-    let seriesList2 = [];
+    const seriesList1 = [];
+    const seriesList2 = [];
     let seriesKey;
 
-    for(let i = 0; i < Math.min(seriesItems.length, SERIES_ITEM_COUNT_PER_ROW); i++) {
-
+    for (let i = 0; i < Math.min(seriesItems.length, SERIES_ITEM_COUNT_PER_ROW); i++) {
       seriesKey = seriesItems[i].key;
       seriesList1.push(
-        <div className="col-lg-1 col-md-2 col-sm-2 col-xs-2" key={`productItem_${i}`}>
+        <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2" key={`productItem_${i}`}>
           <Link to={`product/${categoryKey}/${seriesKey}`}>
             <p>{`-${seriesItems[i].shortName} Series`}</p>
           </Link>
@@ -30,9 +30,9 @@ export class ProductItem extends Component {
       );
     }
 
-    for(let i = SERIES_ITEM_COUNT_PER_ROW; i < seriesItems.length; i++) {
+    for (let i = SERIES_ITEM_COUNT_PER_ROW; i < seriesItems.length; i++) {
       seriesList2.push(
-        <div className="col-lg-1 col-md-2 col-sm-2 col-xs-2" key={`productItem_${i}`}>
+        <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2" key={`productItem_${i}`}>
           <Link to={`product/${categoryKey}/${seriesKey}`}>
             <p>{`-${seriesItems[i].shortName} Series`}</p>
           </Link>
@@ -43,19 +43,23 @@ export class ProductItem extends Component {
     // if seriresList length greater than 6
     return seriesItems.length <= SERIES_ITEM_COUNT_PER_ROW ? (
       <div>
-        <div className="product-series row flex-center">
+        <div className="product-series row">
           {seriesList1}
         </div>
       </div>
     ) : (
       <div>
-        <div className="product-series row flex-center">
-          {seriesList1}
+        <div className="product-series row">
+          <div className="product-series-container">
+            {seriesList1}
+          </div>
         </div>
-        <div className="product-series row flex-center">
-          {seriesList2}
+        <div className="product-series row">
+          <div className="product-series-container">
+            {seriesList2}
+          </div>
         </div>
-      </div>  
+      </div>
     );
   }
 
@@ -65,10 +69,10 @@ export class ProductItem extends Component {
     const type = this.props.type;
     const className = `product-info${type}`;
 
-    return(
+    return (
       <div className="product-item">
         <Link to={`/edit/category/${categoryIndex}`}>
-          <div className={`${className} flex-center`}>
+          <div className={`${className}`}>
               <h3 className="product-info-name">{`- ${name} -`}</h3>
               <div className="product-img">
                 <ImgItem imgItem={mainImg} />
@@ -81,4 +85,12 @@ export class ProductItem extends Component {
   }
 }
 
-export default ProductItem;
+ProductItem.propTypes = {
+  categoryKey: PropTypes.string,
+  categoryItem: PropTypes.object,
+  type: PropTypes.number,
+  seriesItems: PropTypes.array,
+  categoryIndex: PropTypes.number,
+  name: PropTypes.string,
+  mainImg: PropTypes.object,
+};
