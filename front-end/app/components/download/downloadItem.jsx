2d51d69741a70c00f41af74c3ffa798b03b397e8
Yihau-Chen <a122092487@gmail.com>
2017-07-19 00:25:26 +0800

    typesetting

--- front-end/app/components/download/downloadItem.jsx
+++ front-end/app/components/download/downloadItem.jsx
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ImgItem from 'components/common/imgItem.jsx';
 
-export default class Download extends Component {
-  render () {
-  const {
-    description, link, mainImg, title
-  } = this.props.downloadItem;
-  const downloadItemDescription = description.map((item, index) => <span key={ `downItemDescription_${index}` }>{item}</span>);
-  const downloadItemLink = link.map((item, index) => {
-    return(
-      <li key={'downItemlink_'+index}>
-        <span>{item.key}:</span>
-        <a href={item.url}>Download</a>
-      </li>
-    );
-  });
+export default class DownloadItem extends Component {
+  render() {
+    const {
+      description, link, mainImg, title
+    } = this.props.downloadItem;
+    const downloadItemDescription = description.map((item, index) => <p key={ `downItemDescription_${index}` }>{item}</p>);
+    const downloadItemLink = link.map((item, index) => {
+      return (
+        <li key={`downItemlink_${index}`}>
+          <p>{item.key}:</p>
+          <a href={item.url}>Download</a>
+        </li>
+      );
+    });
     return (
-      <div className="download-item row">
-        <div className="download-img col-lg-3 col-lg-offset-1">
+      <div className="download-item">
+        <div className="download-img col-lg-4 col-md-4 col-sm-4 col-xs-4">
           <ImgItem imgItem={mainImg}/>
         </div>
-        <div className="download-description col-lg-4">
-          <span className="download-description-title"><i className="glyphicon glyphicon-file" />{title}</span>
+        <div className="download-description col-lg-4 col-md-4 col-sm-4 col-xs-4">
+          <h4 className="download-description-title">
+            <i className="glyphicon glyphicon-file" />
+              { title }
+          </h4>
           <ul className="list-unstyled">
-            {downloadItemDescription}
+            { downloadItemDescription }
           </ul>
         </div>
-        <div className="download-link col-lg-3">
-          <i className="glyphicon glyphicon-download-alt"></i>
+        <div className="download-link col-lg-4 col-md-4 col-sm-4 col-xs-4">
+          <i className="glyphicon glyphicon-download-alt" />
           <ul className="list-unstyled">
             {downloadItemLink}
           </ul>
         </div>
       </div>
     );
-  }  
+  }
 }
+
+DownloadItem.propTypes = {
+  downloadItem: PropTypes.object
+};
--- front-end/app/components/download/index.jsx
+++ front-end/app/components/download/index.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import DownloadItem from './downloadItem.jsx';
 
 export default class Download extends Component {
   render() {
-    const download = this.props.download;
-    const downloadList = download.downloadItem.map((item, index) => <DownloadItem downloadItem={item} key={'downItem_' + index}/>);
+    const { header, subheader, downloadItem } = this.props.download;
+    // const downloadList = download.downloadItem.map((item, index) => <DownloadItem downloadItem={item} key={'downItem_' + index}/>);
+    const downloadItemFirst = downloadItem.length > 0 ? <DownloadItem downloadItem={downloadItem[0]} /> : null;
+    const moreButton = downloadItem.length > 1 ? (
+        <div className="download-more col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <Link>
+            <div className="download-more-button">
+              More
+            </div>
+          </Link>
+        </div>
+      ) : null;
     return (
-      <div id="download" className=".container-fluid">
-        <h2 className="text-uppercase"> {download.header}</h2>
-        <p>{download.subheader}</p>
-        { downloadList }
+      <div id="download">
+        <div className="download-title">
+          <h2>{header}</h2>
+          <p>{subheader}</p>
+        </div>
+        { downloadItemFirst }
+        { moreButton }
       </div>
     );
   }
