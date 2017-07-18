ec22ab1187210aa2f70eb52b744263e691491060
Yihau-Chen <a122092487@gmail.com>
2017-07-18 22:10:00 +0800

    fixed coding style

--- front-end/app/components/download/index.jsx
+++ front-end/app/components/download/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import DownloadItem from './downloadItem.jsx';
+
 export default class Download extends Component {
-  render () {
+  render() {
     const download = this.props.download;
-    const downloadList = download.downloadItem.map((item, index) => <DownloadItem downloadItem={item} key={'downItem_'+index}/>);
+    const downloadList = download.downloadItem.map((item, index) => <DownloadItem downloadItem={item} key={'downItem_' + index}/>);
     return (
       <div id="download" className=".container-fluid">
         <h2 className="text-uppercase"> {download.header}</h2>
@@ -11,5 +12,5 @@ export default class Download extends Component {
         { downloadList }
       </div>
     );
-  }  
+  }
 }
