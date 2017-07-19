ba427785e1328f73a01ec7ef346e4a7f29ed6297
Yihau-Chen <a122092487@gmail.com>
2017-07-20 00:28:58 +0800

    format code

--- front-end/app/components/footer/index.jsx
+++ front-end/app/components/footer/index.jsx
@@ -7,7 +7,7 @@ export default class Footer extends Component {
         <footer className="footer-container">
           <div className="footer-logo">
             <img
-              src="https://firebasestorage.googleapis.com/v0/b/techwell-74c86.appspot.com/o/footer%2FLOGO.png?alt=media&token=8dbbb444-8a94-4ecb-b32e-a0494d809004" 
+              src="https://firebasestorage.googleapis.com/v0/b/techwell-74c86.appspot.com/o/footer%2FLOGO.png?alt=media&token=8dbbb444-8a94-4ecb-b32e-a0494d809004"
               alt="LOGO"
             />
           </div>
