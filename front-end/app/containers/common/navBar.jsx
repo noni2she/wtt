2d8f087468ee80813f195ecbe42f79fcd89e84d0
Chun-MingChen <nick830314@gmail.com>
2017-07-22 00:00:30 +0800

    dropdown box for language switch box

--- front-end/app/containers/common/navBar.jsx
+++ front-end/app/containers/common/navBar.jsx
@@ -21,9 +21,15 @@ export class NavBar extends Component {
   onClickHandler(event) {
     event.preventDefault();
     const { name } = event.target;
-    const { onLocaleChange } = this.props;
 
-    onLocaleChange(name);
+    const dropdownItems = document.getElementsByClassName('navbar-item-dropdown-item');
+    if (dropdownItems) {
+      Array.prototype.forEach.call(dropdownItems, (element) => {
+        element.style = 'display: none;';
+      });
+    }
+
+    this.props.onLocaleChange(name);
   }
 
   currentLocaleDisplay(locales) {
@@ -39,50 +45,6 @@ export class NavBar extends Component {
     }
   }
 
-  // render() {
-  //   const { active, locales } = this.props;
-  //   return (
-  //     <nav className="navbar navbar-default navbar-fixed-top">
-  //       <div className="container">
-  //         <div className="navbar-header">
-  //           <Link to={'/'} className="navbar-brand">
-  //             WTT 後台編輯
-  //           </Link>
-
-  //         </div>
-  //         <div id="navbar" className="navbar-collapse collapse">
-  //           <ul className="nav navbar-nav">
-  //             <li className={ active === NAV_BAR_INDEX ? "active" : ''}><Link to={'/'} >頁面管理</Link></li>
-  //             <li className={ active === NAV_BAR_MESSAGES ? "active" : ''}><Link to={'/messages'} >留言板管理</Link></li>
-  //             <li className="dropdown">
-  //               <Link
-  //                 className="dropdown-toggle"
-  //                 data-toggle="dropdown"
-  //                 role="button"
-  //                 aria-haspopup="true"
-  //                 aria-expanded="false"
-  //               >
-  //                 {this.currentLocaleDisplay(locales)}
-  //                 <span className="caret"></span>
-  //               </Link>
-  //               <ul className="dropdown-menu">
-  //                 <li><a name={LOCALE_TW} onClick={this.onClickHandler}>TW</a></li>
-  //                 <li><a name={LOCALE_EN} onClick={this.onClickHandler}>EN</a></li>
-  //                 <li><a name={LOCALE_JP} onClick={this.onClickHandler}>JP</a></li>
-  //               </ul>
-  //             </li>
-  //           </ul>
-
-  //           <ul className="nav navbar-nav navbar-right">
-  //             <li style={{marginTop: '7px'}}>
-  //               <MessageModal />
-  //             </li>
-  //           </ul>
-  //         </div>
-  //       </div>
-  //     </nav>
-  //   );
-  // }
   renderScrollLink(id, showText) {
     return (
       <ScrollLink
@@ -97,7 +59,17 @@ export class NavBar extends Component {
     );
   }
 
+  showDropdownItem() {
+    const dropdownItems = document.getElementsByClassName('navbar-item-dropdown-item');
+    if (dropdownItems) {
+      Array.prototype.forEach.call(dropdownItems, (element) => {
+        element.style = 'display: initial;';
+      });
+    }
+  }
+
   render() {
+    const { locales } = this.props;
     return (
       <nav id="navbar" className="common-navbar container">
         <div className="navbar-content">
@@ -116,7 +88,16 @@ export class NavBar extends Component {
             <li className="navbar-content-item navbar-content-item-pc">{this.renderScrollLink('news', 'NEWS')}</li>
             <li className="navbar-content-item navbar-content-item-pc">{this.renderScrollLink('download', 'DOWNLOAD')}</li>
             <li className="navbar-content-item navbar-content-item-pc">{this.renderScrollLink('contact', 'CONTACT')}</li>
-            <li className="navbar-content-item navbar-content-item-pc"><a>EN</a></li>
+            <li className="navbar-content-item navbar-content-item-pc navbar-item-dropdown">
+              <div onMouseEnter={this.showDropdownItem}>
+                <a>{this.currentLocaleDisplay(locales)}</a>
+                <span className="drop-icon glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+              </div>
+
+              <div className="navbar-item-dropdown-item" onClick={this.onClickHandler}><a name={LOCALE_TW}>TW</a></div>
+              <div className="navbar-item-dropdown-item" onClick={this.onClickHandler}><a name={LOCALE_JP}>JP</a></div>
+              <div className="navbar-item-dropdown-item" onClick={this.onClickHandler}><a name={LOCALE_EN}>EN</a></div>
+            </li>
 
             <li className="navbar-content-item navbar-content-item-mweb"><a>ABOUT US</a></li>
             <li className="navbar-content-message">
@@ -137,7 +118,7 @@ NavBar.propTypes = {
 const mapStateToProps = ({locales}) => {
   return {
     locales,
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps, { onLocaleChange })(NavBar);
