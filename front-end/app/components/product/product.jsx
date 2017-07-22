1df043cbabaad4b5fd2fa2e16358fba26b7d956f
Chun-MingChen <nick830314@gmail.com>
2017-07-22 15:46:20 +0800

    tunning layout for product

--- front-end/app/components/product/product.jsx
+++ front-end/app/components/product/product.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import ImgItem from 'components/common/imgItem.jsx';
-import { SERIES_ITEM_COUNT_PER_ROW } from 'constants/common';
 
 export default class ProductItem extends Component {
 
@@ -16,65 +15,35 @@ export default class ProductItem extends Component {
     const { categoryKey } = this.props;
     const { seriesItems } = this.props.categoryItem;
 
-    let seriesList1 = [];
-    let seriesList2 = [];
-    let seriesKey;
-
-    for (let i = 0; i < Math.min(seriesItems.length, SERIES_ITEM_COUNT_PER_ROW); i++) {
-      seriesKey = seriesItems[i].key;
-      seriesList1.push(
-        <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2" key={`productItem_${i}`}>
+    const series = seriesItems.map((seriesItem, index) => {
+      const seriesKey = seriesItem.key;
+      return (
+        <div className="product-series-item col-lg-2 col-md-3 col-sm-3 col-xs-3" key={`productItem_${categoryKey}_${index}`}>
           <Link to={`product/${categoryKey}/${seriesKey}`}>
-            <p>{`-${seriesItems[i].shortName} Series`}</p>
+            <p>{`-${seriesItem.shortName} Series`}</p>
           </Link>
         </div>
       );
-    }
+    });
 
-    for (let i = SERIES_ITEM_COUNT_PER_ROW; i < seriesItems.length; i++) {
-      seriesKey = seriesItems[i].key;
-      seriesList2.push(
-        <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2" key={`productItem_${i}`}>
-          <Link to={`product/${categoryKey}/${seriesKey}`}>
-            <p>{`-${seriesItems[i].shortName} Series`}</p>
-          </Link>
-        </div>
-      );
-    }
-
-    // if seriresList length greater than 6
-    return seriesItems.length <= SERIES_ITEM_COUNT_PER_ROW ? (
-      <div>
-        <div className="product-series1 row">
-          <div className="product-series-container-1">
-            {seriesList1}
-          </div>
-        </div>
-      </div>
-    ) : (
-      <div>
-        <div className="product-series row">
-          <div className="product-series-container-1">
-            {seriesList1}
-          </div>
-        </div>
-        <div className="product-series row">
-          <div className="product-series-container-2">
-            {seriesList2}
-          </div>
+    return (
+      <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 product-series">
+        <div className="row col-lg-6 col-md-12 col-sm-12 col-xs-12 col-lg-offset-3 product-series-container">
+          {series}
         </div>
       </div>
     );
   }
 
   render() {
-    const { categoryIndex, type } = this.props;
-    const { name, mainImg } = this.props.categoryItem;
+    const { categoryKey, type } = this.props;
+    const { name, mainImg, seriesItems } = this.props.categoryItem;
     const className = `product-info${type}`;
+    const seriesKey = seriesItems[0].key;
 
     return (
-      <div className="product-item">
-        <Link to={`/edit/category/${categoryIndex}`}>
+      <div className="product-item container-fluid">
+        <Link to={`product/${categoryKey}/${seriesKey}`}>
           <div className={`${className}`}>
               <h3 className="product-info-name">{`- ${name} -`}</h3>
               <div className="product-img">
