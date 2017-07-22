9ca4ff533aa2555f6711b9a52a46db0c137edaba
Chun-MingChen <nick830314@gmail.com>
2017-07-22 14:39:03 +0800

    move hashLinkScroll function to utils/common

--- front-end/app/main.js
+++ front-end/app/main.js
@@ -20,6 +20,9 @@ import 'stylesheet/index.scss';
 import 'jquery';
 import 'bootstrap';
 
+// function
+import { hashLinkScroll } from 'utils/common';
+
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
@@ -33,20 +36,6 @@ const store = createStore(
 sagaMiddleware.run(rootSaga);
 
 bootstrap(store)();
-
-function hashLinkScroll() {
-  const { hash } = window.location;
-  if (hash !== '') {
-    // Push onto callback queue so it runs after the DOM is updated,
-    // this is required when navigating from a different page so that
-    // the element is rendered on the page before trying to getElementById.
-    setTimeout(() => {
-      const id = hash.replace('#', '');
-      const element = document.getElementById(id);
-      if (element) element.scrollIntoView();
-    }, 0);
-  }
-}
 
 ReactDOM.render(
   <Provider store={store}>
