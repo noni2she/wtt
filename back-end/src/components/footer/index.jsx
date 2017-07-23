fbce98a6637ae96cabc7f242f51bca070fed3174
Yihau-Chen <a122092487@gmail.com>
2017-07-23 16:01:55 +0800

    sync footer

--- back-end/src/components/footer/index.jsx
+++ back-end/src/components/footer/index.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Footer extends Component {
-  render () {
-    return(
-      <div id="footer" className="container-fluid" />
+  render() {
+    return (
+      <div id="footer">
+        <footer className="footer-container">
+          <div className="footer-logo">
+            <img
+              src="footer/logo.png"
+              alt="LOGO"
+            />
+          </div>
+          <div className="footer-description">
+            <p>2017 Techwell industried co.,ltd. All</p>
+          </div>
+        </footer>
+      </div>
     );
-  }  
+  }
 }
