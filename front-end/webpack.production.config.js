4c15bc4c1f26db1ac97ad3a76d7cf9c8441b2bbd
Chun-MingChen <nick830314@gmail.com>
2017-07-23 17:39:16 +0800

    active production mode

--- front-end/webpack.production.config.js
+++ front-end/webpack.production.config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var path = require('path');
+var APP = path.join(__dirname, 'app');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
@@ -16,13 +16,11 @@ module.exports = {
     publicPath: '/'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new HtmlWebpackPlugin({
-      template: 'app/index.tpl.html',
+      template: 'app/index.html',
       inject: 'body',
       filename: 'index.html'
     }),
-    new ExtractTextPlugin('[name]-[hash].min.css'),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false,
@@ -35,31 +33,74 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
+    }),
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery'
+    }),
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
+    rules: [{
+      test: /\.(js|jsx)$/,
+      loader: require.resolve('babel-loader'),
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        "presets": ["es2015", "stage-0", "react"]
-      }
-    }, {
-      test: /\.json?$/,
-      loader: 'json'
     }, {
       test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style', 'css?modules&localIdentName=[name]---[local]---[hash:base64:5]!postcss')
+      use: [
+        require.resolve('style-loader'),
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            importLoaders: 1,
+          },
+        },
+        {
+          loader: require.resolve('postcss-loader'),
+          options: {
+            ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+            plugins: () => [
+              require('postcss-flexbugs-fixes'),
+              autoprefixer({
+                browsers: [
+                  '>1%',
+                  'last 4 versions',
+                  'Firefox ESR',
+                  'not ie < 9', // React doesn't support IE8 anyway
+                ],
+                flexbox: 'no-2009',
+              }),
+            ],
+          },
+        },
+      ],
+    }, {
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+      }, {
+          loader: "css-loader" // translates CSS into CommonJS
+      }, {
+          loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+      loader: 'url-loader?limit=100000'
     }]
   },
-  postcss: [
-    require('autoprefixer')
-  ],
   resolve: {
-    root: [
-      path.resolve('./app'),
-      path.resolve('./test'),
-    ],
+    extensions: ['.js', '.json', '.jsx'],
+    alias: {
+      actions: path.join(APP, 'actions'),
+      components: path.join(APP, 'components'),
+      constants: path.join(APP, 'constants'),
+      containers: path.join(APP, 'containers'),
+      reducers: path.join(APP, 'reducers'),
+      routes: path.join(APP, 'routes'),
+      sagas: path.join(APP, 'sagas'),
+      utils: path.join(APP, 'utils'),
+      img: path.join(APP, 'assets', 'img'),
+      stylesheet: path.join(APP, 'assets', 'stylesheet'),
+    },
   }
 };
